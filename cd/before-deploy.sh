8b387f3bd9c0f177c87f70833156988e8d1368dd
Jose Quijada <joquijada2010@gmail.com>
2019-10-06 21:09:18 -0400

    feat(ci-cd): set up, address `bad decrypt`

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_76d829fd1795_key -iv $encrypted_76d829fd1795_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_67304408949c_key -iv $encrypted_67304408949c_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
